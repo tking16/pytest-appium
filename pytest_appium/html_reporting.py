f2b5290c184dcd373f002f1effbe1db91ab55aa5
theo.king <theo.king@global.com>
2024-06-04 17:58:25 +0100

    Using pass instead of return for when video fails so actual error if any can still be displayed in report

--- pytest_appium/html_reporting.py
+++ pytest_appium/html_reporting.py
@@ -37,7 +37,7 @@ def _gather_video(item, report, driver, summary, extra):
         video = driver.stop_recording_screen()
     except Exception as e:
         summary.append('WARNING: Failed to gather video: {0}'.format(e))
-        return
+        pass
     pytest_html = item.config.pluginmanager.getplugin('html')
     if video and pytest_html is not None:
         # add screenshot to the html report
