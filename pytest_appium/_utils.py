5fb137719069f05776ffcf9fc309b3a99862542f
theo.king <theo.king@global.com>
2026-07-30 11:43:48 +0100

    Addressing PR comments from 2024 - updating post_json

--- pytest_appium/_utils.py
+++ pytest_appium/_utils.py
@@ -1,16 +1,5 @@
-import json
-import urllib  #.request
 import time
 import requests
-
-
-def _decode_response(response):
-    data = response.read()
-    try:
-        encoding = response.info().get_content_charset('utf-8')
-    except AttributeError:
-        encoding = 'utf-8'
-    return json.loads(data.decode(encoding))
 
 
 def get_json(url, headers: dict=None):
@@ -18,19 +7,9 @@ def get_json(url, headers: dict=None):
     return r.json()
 
 
-def post_json(url, data):
-    try:
-        return _decode_response(
-            urllib.request.urlopen(
-                urllib.request.Request(
-                    url,
-                    data=json.dumps(data).encode('utf8'),
-                    headers={'content-type': 'application/json'},
-                )
-            )
-        )
-    except urllib.error.HTTPError as ex:
-        return _decode_response(ex)
+def post_json(url, data, headers: dict=None):
+    r = requests.post(url, json=data, headers=headers)
+    return r.json()
 
 
 def wait_for(
